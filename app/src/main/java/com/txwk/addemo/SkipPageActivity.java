a7879ba33cb931e9bb496fc8951843f969ded92f
senonwx <1083911695@qq.com>
2019-06-03 18:21:45 +0800

    first commit

--- app/src/main/java/com/txwk/addemo/SkipPageActivity.java
+++ app/src/main/java/com/txwk/addemo/SkipPageActivity.java
@@ -4,9 +4,14 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.zhouwei.mzbanner.MZBannerView;
@@ -21,7 +26,9 @@ import java.util.List;
 public class SkipPageActivity extends AppCompatActivity {
 
     private MZBannerView banner;
-    private List<String> tipList;
+    private ViewPager viewPager;
+    private List<String> tipList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +36,8 @@ public class SkipPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_skip_page);
 
         banner = findViewById(R.id.banner);
-        tipList = new ArrayList<>();
-
         tipList.addAll(Arrays.asList(new String[]{"广告1", "广告2", "广告3"}));
+
         // 设置数据
         banner.setPages(tipList, new MZHolderCreator<BannerViewHolder>() {
             @Override
@@ -40,6 +46,10 @@ public class SkipPageActivity extends AppCompatActivity {
             }
         });
         banner.setDuration(3);
+
+//        viewPager = findViewById(R.id.viewPager);
+//        viewPager.setAdapter(new PagerAdapte());
+
     }
 
     public void back(View view) {
@@ -53,6 +63,7 @@ public class SkipPageActivity extends AppCompatActivity {
 
         finish();
     }
+
 
     public class BannerViewHolder implements MZViewHolder<String> {
         private TextView banner_tv;
@@ -85,6 +96,36 @@ public class SkipPageActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         banner.start();//开始轮播
+    }
+
+
+    class PagerAdapte extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return tipList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+            return view == o;
+        }
+
+        @SuppressLint("ResourceAsColor")
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            TextView banner_tv = new TextView(SkipPageActivity.this);
+            banner_tv.setText(tipList.get(position));
+            banner_tv.setGravity(Gravity.CENTER);
+            banner_tv.setTextSize(30);
+            banner_tv.setBackgroundColor(R.color.colorPrimaryDark);
+            container.addView(banner_tv);
+            return banner_tv;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 
 }
